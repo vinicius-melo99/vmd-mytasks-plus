12f53eefc09a3af88070ee0c7646809363812958
Vinicius Melo <viniciusmelo317@gmail.com>
2025-02-28 18:39:47 -0300

    feat: criada seção de exibição de tarefas + estilização

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
+import { FiShare2 } from 'react-icons/fi';
+import { FaTrash } from 'react-icons/fa';
 import styles from './styles.module.css';
 import Head from 'next/head';
 import Textarea from '@/components/Textarea';
@@ -30,6 +32,36 @@ const Dashboard = () => {
               <button className={styles.submitButton}>Registrar</button>
             </form>
           </div>
+        </section>
+
+        <section className={styles.taskContainer}>
+          <h1>Minhas Tarefas</h1>
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PÚBLICA</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+
+            <div className={styles.taskContent}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Tempora, enim. Lorem ipsum, dolor sit amet consectetur
+                adipisicing elit. Explicabo, aliquam! Modi sint aspernatur
+                consequuntur, suscipit itaque quia laboriosam inventore totam
+                maiores eius quibusdam dolores aliquid minima laborum a non ut?
+                Earum qui quod assumenda atque tempore accusamus culpa
+                voluptatem reiciendis! Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Cum officiis voluptas perferendis fugiat aut,
+                porro placeat unde eos magnam et molestiae nisi quisquam
+                perspiciatis animi quae. Aut deleniti voluptas corporis?
+              </p>
+              <button className={styles.trashButton}>
+                <FaTrash color="red" size={24} />
+              </button>
+            </div>
+          </article>
         </section>
       </main>
     </>
