64e3524f7e89c7a1c028798ebc9ec031b5c4740b
Vinicius Melo <viniciusmelo317@gmail.com>
2025-02-28 15:24:43 -0300

    feat: criação do dashboard pt1 - criação e estilização de componentes de formulário

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
 import styles from './styles.module.css';
 import Head from 'next/head';
+import Textarea from '@/components/Textarea';
 
 const Dashboard = () => {
   return (
@@ -9,9 +10,28 @@ const Dashboard = () => {
       <Head>
         <title>Meu Painel de Tarefas</title>
       </Head>
-      <div className={styles.container}>
-        <h1>Dashboard</h1>
-      </div>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual a sua tarefa?</h1>
+
+            <form>
+              <Textarea placeholder="Digite a sua tarefa..." />
+              <div className={styles.checkboxArea}>
+                <input
+                  id="public-task"
+                  type="checkbox"
+                  className={styles.checkbox}
+                />
+                <label htmlFor="public-task" className={styles.label}>
+                  Deixar tarefa pública?
+                </label>
+              </div>
+              <button className={styles.submitButton}>Registrar</button>
+            </form>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
