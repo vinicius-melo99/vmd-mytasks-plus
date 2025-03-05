b09b5309af861b3c482f5ab28731e9536a769d34
Vinicius Melo <viniciusmelo317@gmail.com>
2025-03-05 14:09:24 -0300

    config: alteração do favicon.ico

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
 import { SessionProvider } from 'next-auth/react';
-import Header from '@/components/Header';
 import type { AppProps } from 'next/app';
-import '@/styles/globals.css';
-import 'react-tooltip/dist/react-tooltip.css';
 import { ToastContainer } from 'react-toastify';
+import Header from '@/components/Header';
+import 'react-tooltip/dist/react-tooltip.css';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <ToastContainer />
-      <Header />
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <SessionProvider session={pageProps.session}>
+        <ToastContainer />
+        <Header />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
